7cffd3374c37669a5aa39c2f896743d9b0441dbe
Tobias V. Langhoff <tobias@langhoff.no>
2022-07-24 20:26:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Reddit Saved Saver
+==================
+
+A simple Python script for converting your saved posts and comments on Reddit to Markdown files with YAML frontmatter metadata.
+
+I made this to dump all my saved Reddit stuff into [Obsidian](https://obsidian.md). It's not at all customizable or user friendly right now.
+
+Usage
+-----
+
+You need to create a Reddit app on https://old.reddit.com/prefs/apps/ and then put the client ID, secret key, as well as your Reddit username and password into `config.ini`.
+
+Make sure `praw` is installed, and then run the script with `python reddit-saved-saver.py`.
+
+The script will create a directory called `reddit` and then make a new subdirectory per subreddit.
---
+++ reddit-saved-saver.py
@@ -0,0 +1,45 @@
+import praw, os, config
+from praw.models import Submission, Comment
+from datetime import datetime
+
+reddit = praw.Reddit(client_id=config.client_id,
+                     client_secret=config.client_secret,
+                     username=config.username,
+                     password=config.password,
+                     user_agent='Reddit Saved Saver by /u/tobiasvl')
+
+print("Fetching...")
+
+try:
+    saved = reddit.user.me().saved(limit=1000)
+except:
+    sys.exit("Failed to find your saved posts, did you add your credentials to config.ini?")
+
+top_dir = 'reddit/'
+
+if not os.path.exists(top_dir):
+    os.mkdir(top_dir)
+
+for submission in saved:
+    sub_dir = top_dir + submission.subreddit.display_name + '/'
+    if not os.path.exists(sub_dir):
+        os.mkdir(sub_dir)
+    with open(sub_dir + submission.id + '.md', 'w', encoding="utf-8") as f:
+        f.write('---\n')
+        f.write('id: ' + submission.id + '\n')
+        try:
+            f.write('author: /u/' + submission.author.name + '\n')
+        except:
+            f.write('author: [deleted]\n')
+        f.write('timestamp: ' + str(datetime.utcfromtimestamp(submission.created_utc)) + '\n')
+        f.write('permalink: https://reddit.com' + submission.permalink + '\n')
+        f.write('subreddit: /r/' + submission.subreddit.display_name + '\n')
+        f.write('---\n\n')
+        if isinstance(submission, Submission):
+            f.write('# ' + submission.title + '\n\n')
+            if submission.is_self:
+                f.write(submission.selftext)
+            else:
+                f.write(submission.url)
+        elif isinstance(submission, Comment):
+            f.write(submission.body)
